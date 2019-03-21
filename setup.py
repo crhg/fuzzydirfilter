05f2e26ca89b74c7cebf9ad0c3a3e074e6649ae9
Manabu Matsui <manabu.matsui@gmail.com>
2019-03-21 09:14:19 +0900

    version 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fuzzydirfilter',
-    version='1.1',
+    version='1.1.0',
     packages=find_packages(),
     install_requires=['fuzzywuzzy', 'python-Levenshtein'],
     entry_points={
