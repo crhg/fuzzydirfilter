4eb17bda792b78ff8f3682cf9b193c22a754419c
Manabu Matsui <manabu.matsui@gmail.com>
2019-03-21 13:19:46 +0900

    don't filter if sort

--- fuzzydirfilter/main.py
+++ fuzzydirfilter/main.py
@@ -9,27 +9,41 @@ DEFAULT_THRESHOLD = 70
 
 
 def fuzzydirfilter_main():
-    default_threshold = os.getenv('FUZZYDIRFILTER_THRESHOLD', DEFAULT_THRESHOLD)
-
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--sort', action='store_true')
-    parser.add_argument('--threshold', type=int, default=default_threshold)
+    parser.add_argument('--threshold', type=int, default=None)
     parser.add_argument('search_string', type=str)
     parser.add_argument('file', nargs='*', type=argparse.FileType('r'), default=[sys.stdin])
 
     args = parser.parse_args()
 
-    entries = []
-    for file in args.file:
+    if args.sort:
+        do_sort(args.search_string, args.file)
+    else:
+        threshold = args.threshold
+        if threshold is None:
+            threshold = os.getenv('FUZZYDIRFILTER_THRESHOLD', DEFAULT_THRESHOLD)
+
+        do_filter(args, args.file, threshold)
+
+
+def do_filter(search_string, files, threshold):
+    for file in files:
         for line in file:
             directory_name = line.rstrip().split("/")[-1]
-            score = fuzz.partial_ratio(args.search_string, directory_name)
-            if score >= args.threshold:
-                if args.sort:
-                    entries.append((score, line))
-                else:
-                    print(line, end='')
+            score = fuzz.partial_ratio(search_string, directory_name)
+            if score >= threshold:
+                print(line, end='')
 
-    if args.sort:
-        for e in sorted(entries, key=itemgetter(0)):
-            print(e[1], end='')
+
+def do_sort(search_string, files):
+    entries = []
+    for file in files:
+        for line in file:
+            directory_name = line.rstrip().split("/")[-1]
+            score = fuzz.ratio(search_string, directory_name)
+            entries.append((score, line))
+
+    for e in sorted(entries, key=itemgetter(0)):
+        print(e[1], end='')
+
